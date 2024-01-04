14f8ea3bf0697d0a9fcfa1b1814282ede2a81cc6
unknown <kkamesh2309@gmail.com>
2024-01-04 11:59:11 +0530

    Pushing both changes

---
+++ src/components/DeleteModal.js
@@ -0,0 +1,27 @@
+
+const DeleteModal = ({onClick,name,id,type,index}) => {
+    return (
+        <div>
+            <div className="modal fade" id={index} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className=" modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header position-relative">
+                            <h5 className="modal-title position-absolute delete-modal fw-bold fst-italic textcolor text-danger " id="staticBackdropLabel">{id}-{name}</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body fw-bold fst-italic textcolor">
+                            Are you sure u want to Delete {type}..
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-outline-danger fw-bold fst-italic textcolor" onClick={onClick} data-bs-dismiss="modal">Delete</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default DeleteModal
